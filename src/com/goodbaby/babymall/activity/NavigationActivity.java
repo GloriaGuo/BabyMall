ba80d27ef7ff009cb8ff71a67cfdc38d24476108
ax2024 <fengxiao2024@gmail.com>
2013-05-24 18:05:32 +0800

    add tabs.

--- src/com/goodbaby/babymall/activity/NavigationActivity.java
+++ src/com/goodbaby/babymall/activity/NavigationActivity.java
@@ -1,16 +1,21 @@
 package com.goodbaby.babymall.activity;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+
 import com.goodbaby.babymall.R;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-public class NavigationActivity extends Activity {
+public class NavigationActivity extends FragmentActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE); 
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title); 
+
     }
 
 }
